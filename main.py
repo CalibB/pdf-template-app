b5942f0bb3ad2c62e8708823c1bf70216bb67e21
CalibB <110089911+CalibB@users.noreply.github.com>
2023-06-22 19:13:57 +0200

    feat: added page lines to each page

--- main.py
+++ main.py
@@ -19,16 +19,33 @@ for index, row in df.iterrows():
     # Set Footer
     pdf.set_font_size(10)
     pdf.set_text_color(180, 180, 180)
-    pdf.ln(273)
+    pdf.ln(278)
     pdf.cell(w=0, txt=row['Topic'], align='R')
     pages = pages - 1
+
+    # Page lines
+    starting_y_position = 32
+    pdf.set_draw_color(180, 180, 180)
+    pdf.line(10, starting_y_position, 200, starting_y_position)
+    for i in range(0, 25):
+        starting_y_position = starting_y_position + 10
+        pdf.line(10, starting_y_position, 200, starting_y_position)
+
     for pages in range(pages):
         pdf.add_page()
 
         # Set Footer
         pdf.set_font_size(10)
         pdf.set_text_color(180, 180, 180)
-        pdf.ln(273)
+        pdf.ln(278)
         pdf.cell(w=0, txt=row['Topic'], align='R')
+
+        # Page lines
+        starting_y_position = 32
+        pdf.set_draw_color(180, 180, 180)
+        pdf.line(10, starting_y_position, 200, starting_y_position)
+        for i in range(0, 25):
+            starting_y_position = starting_y_position + 10
+            pdf.line(10, starting_y_position, 200, starting_y_position)
 
 pdf.output(name='test.pdf')
